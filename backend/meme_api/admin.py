614a160547c70cf18b74bde2881f84f95ffeae65
koerners <contact@skoerner.com>
2020-12-21 22:39:54 +0100

    Implemented an Adminpanel to access and manipulate all data (#106)

--- backend/meme_api/admin.py
+++ backend/meme_api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from meme_api.models import Meme, Comment, Vote
+
+
+
+
+admin.site.register(Meme)
+admin.site.register(Comment)
+admin.site.register(Vote)
