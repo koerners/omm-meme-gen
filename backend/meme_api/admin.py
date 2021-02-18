18713e812d30e94abcc087d355bf3e80f0243c50
boehm-projects <boehmmo@cip.ifi.lmu.de>
2021-02-18 23:32:30 +0100

    Pen

--- backend/meme_api/admin.py
+++ backend/meme_api/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from meme_api.models import Meme, Comment, Vote
-
-
-
+from meme_api.models import Meme, Comment, Vote, Blocker
 
 admin.site.register(Meme)
 admin.site.register(Comment)
 admin.site.register(Vote)
+admin.site.register(Blocker)
