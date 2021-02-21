9c62b3925328815cc6a11a8a9db1b29572ecfa94
Denis Strauch <denis.strauch@campus.lmu.de>
2021-02-21 15:06:46 +0100

    add screenReader for my-memes

--- frontend/src/app/my-memes/my-memes.component.ts
+++ frontend/src/app/my-memes/my-memes.component.ts
@@ -3,7 +3,8 @@ import {BreakpointObserver} from '@angular/cdk/layout';
 import {MemeService} from '../services/meme.service';
 import {Meme} from '../Meme';
 import {PageEvent} from '@angular/material/paginator';
-import { MatTabChangeEvent } from '@angular/material/tabs';
+import {MatTabChangeEvent} from '@angular/material/tabs';
+import {SpeechService} from '../services/speech.service';
 
 @Component({
   selector: 'app-my-memes',
@@ -17,9 +18,12 @@ export class MyMemesComponent implements OnInit {
   currentPage = 0;
   nextUrl: string;
   prevUrl: string;
+  screenReaderText: Map<string, string>;
 
-  constructor(private memeService: MemeService) {
-
+  constructor(private memeService: MemeService, private speechService: SpeechService) {
+    this.screenReaderText = new Map<string, string>();
+    // this.screenReaderText.set('', '');
+    this.screenReaderText.set('Welcome', 'This page is Meme Life Gallery.');
   }
 
   ngOnInit(): void {
@@ -31,6 +35,10 @@ export class MyMemesComponent implements OnInit {
   }
 
   getData($event: PageEvent): any {
+    this.screenReaderText.set('Paging', 'You are on page ' + ($event.pageIndex + 1) +
+      ' of ' + (Math.floor(this.memeLength / 6) + 1) +
+      ', showing ' + ($event.pageIndex < Math.floor(this.memeLength / 6) ? 6 : (this.memeLength % 6)) + ' memes.');
+    this.readCurrentMemeSet();
     if ($event.pageIndex > this.currentPage) {
       this.memeService.paginator(this.nextUrl).subscribe(data => {
         this.allMemes = data.results;
@@ -56,23 +64,35 @@ export class MyMemesComponent implements OnInit {
 
   loadMemes(what: number): any {
     if (what === 0){
+      this.screenReaderText.set('WhatPage', 'You´re watching public memes from all users.');
       this.memeService.getAll().subscribe(data => {
           this.allMemes = data.results;
           this.memeLength = data.count;
           this.nextUrl = data.next;
           this.prevUrl = data.previous;
           console.log(this.nextUrl, this.prevUrl);
+
+          this.screenReaderText.set('Paging', 'You are on page ' + 1 +
+            ' of ' + (Math.floor(this.memeLength / 6) + 1) +
+            ', showing ' + (this.memeLength >= 6 ? 6 : (this.memeLength % 6)) + ' memes.');
+          this.readCurrentMemeSet();
           return this.allMemes;
         }
       );
     }
     if (what === 1){
+      this.screenReaderText.set('WhatPage', 'You´re watching your own memes.');
       this.memeService.getOwn().subscribe(data => {
           this.allMemes = data.results;
           this.memeLength = data.count;
           this.nextUrl = data.next;
           this.prevUrl = data.previous;
           console.log(this.nextUrl, this.prevUrl);
+
+          this.screenReaderText.set('Paging', 'You are on page ' + 1 +
+            ' of ' + (Math.floor(this.memeLength / 6) + 1) +
+            ', showing ' + (this.memeLength >= 6 ? 6 : (this.memeLength % 6)) + ' memes.');
+          this.readCurrentMemeSet();
           return this.allMemes;
         }
       );
@@ -83,5 +103,42 @@ export class MyMemesComponent implements OnInit {
     this.currentPage = 0;
     this.loadMemes($event.index);
 
+  }
+
+  // ScreenReader and its helper functions //
+  public screenReader(): void {
+    this.speechService.speak(this.screenReaderBuilder());
+  }
+
+  private screenReaderBuilder(): string {
+    let text = '';
+    text += this.screenReaderText.get('Welcome') + ' ';
+    text += (this.screenReaderText.get('WhatPage') || '') + ' ';
+    text += (this.screenReaderText.get('Amount') || '') + ' ';
+    text += (this.screenReaderText.get('Paging') || '') + ' ';
+    text += (this.screenReaderText.get('MemeTitles') || '') + ' ';
+    return text;
+  }
+
+  private readCurrentMemeSet(): void {
+    this.screenReaderText.set('Amount', this.memeLength + 'Memes are available.');
+    let memeTitles = 'The memes are called: ';
+    let countEmpty = 0;
+    this.allMemes.forEach(element => {
+      if (element.title) {
+        memeTitles += element.title + ' - ';
+      }
+      else {
+        countEmpty++;
+      }
+    });
+    if (countEmpty > 0) {
+      memeTitles += 'There are ' + countEmpty + ' memes with no title.';
+    }
+    this.screenReaderText.set('MemeTitles', memeTitles);
+  }
+
+  public stopScreenReader(): void {
+    this.speechService.stop();
   }
 }
