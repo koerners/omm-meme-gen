d0c913e012ef25f161b917d876315544a0fd552f
Flowi Laptop <fklement92@gmail.com>
2021-02-25 21:27:26 +0100

    fix missing comma

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -96,7 +96,7 @@ import { ChartsModule } from 'ng2-charts';
     ShareButtonsModule,
     ShareIconsModule,
     MatDialogModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
     ChartsModule
   ],
   providers: [
