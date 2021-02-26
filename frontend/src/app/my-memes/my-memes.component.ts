79bd7b7c7a4cee8d783f98f5eb098698d5b6d889
Denis Strauch <denis.strauch@campus.lmu.de>
2021-02-26 17:18:30 +0100

    basic filter & ordering on all_memes

--- frontend/src/app/my-memes/my-memes.component.ts
+++ frontend/src/app/my-memes/my-memes.component.ts
@@ -8,6 +8,11 @@ import {MemeService} from '../services/meme.service';
 import {SpeechService} from '../services/speech.service';
 import {VoiceRecognitionService} from '../services/voice-recognition.service';
 
+interface TextType {
+  text: string;
+  type: string;
+}
+
 @Component({
   selector: 'app-my-memes',
   templateUrl: './my-memes.component.html',
@@ -17,26 +22,55 @@ export class MyMemesComponent implements OnInit {
   allMemes: Meme[];
   pageEvent: PageEvent;
   memeLength: number;
-  currentPage = 0;
+  currentTab: number;
+  currentPage: number;
   nextUrl: string;
   prevUrl: string;
   screenReaderText: Map<string, string>;
 
+  // using | keyvalue: asIsOrder for iteration keeps the order
+  filterOptions: Map<string, TextType> = new Map([
+    ['search', {text: 'Text', type: 'text'}],
+    ['owner_id', {text: 'Username', type: 'user'}],
+    ['-views', {text: 'View Count lte', type: 'number'}],
+    ['views', {text: 'View Count gte', type: 'number'}],
+    ['-created', {text: 'Memes created before', type: 'date'}],
+    ['created', {text: 'Memes created after', type: 'date'}],
+  ]);
+  selectedFilter: string;
+  filterType: string;
+  filterValue: string;
+
+  sortOptions: Map<string, string> = new Map([
+    ['title', 'Title'],
+    ['owner', 'Username'],
+    ['views', 'View Count'],
+    ['created', 'Timestamp'],
+  ]);
+  selectedSort: string;
+  selectedOrder: string;
+
   constructor(private memeService: MemeService, private ngZone: NgZone, private router: Router,
               private speechService: SpeechService, public voiceRecognitionService: VoiceRecognitionService) {
+    this.currentTab = 0;
+    this.currentPage = 0;
+    this.selectedSort = 'created';
+    this.selectedOrder = '-';
+    this.selectedFilter = '';
+    this.filterType = '';
+    this.filterValue = '';
+
     this.screenReaderText = new Map<string, string>();
     this.screenReaderText.set('Welcome', 'This page is Meme Life Gallery.');
     this.initVoiceRecognitionCommands();
   }
 
   ngOnInit(): void {
-    this.loadMemes(0);
+    this.loadMemes();
   }
 
   getData($event: PageEvent): any {
-    this.screenReaderText.set('Paging', 'You are on page ' + ($event.pageIndex + 1) +
-      ' of ' + (Math.floor(this.memeLength / 6) + 1) +
-      ', showing ' + ($event.pageIndex < Math.floor(this.memeLength / 6) ? 6 : (this.memeLength % 6)) + ' memes.');
+    this.readPaging($event.pageIndex + 1);
     if ($event.pageIndex > this.currentPage) {
       this.memeService.paginator(this.nextUrl).subscribe(data => {
         this.allMemes = data.results;
@@ -62,25 +96,23 @@ export class MyMemesComponent implements OnInit {
 
   }
 
-  loadMemes(what: number): any {
-    if (what === 0){
+  private loadMemes(): any {
+    if (this.currentTab === 0) {
       this.screenReaderText.set('WhatPage', 'You are watching public memes from all users.');
-      this.memeService.getAll().subscribe(data => {
+      this.memeService.getAllSortFilter(this.selectedSort, this.selectedOrder, this.selectedFilter, this.filterValue).subscribe(data => {
           this.allMemes = data.results;
           this.memeLength = data.count;
           this.nextUrl = data.next;
           this.prevUrl = data.previous;
           // console.log(this.nextUrl, this.prevUrl);
 
-          this.screenReaderText.set('Paging', 'You are on page ' + 1 +
-            ' of ' + (Math.floor(this.memeLength / 6) + 1) +
-            ', showing ' + (this.memeLength >= 6 ? 6 : (this.memeLength % 6)) + ' memes.');
+          this.readPaging(1);
           this.readCurrentMemeSet();
           return this.allMemes;
         }
       );
     }
-    if (what === 1){
+    else {
       this.screenReaderText.set('WhatPage', 'You are watching your own memes.');
       this.memeService.getOwn().subscribe(data => {
           this.allMemes = data.results;
@@ -89,9 +121,7 @@ export class MyMemesComponent implements OnInit {
           this.prevUrl = data.previous;
           // console.log(this.nextUrl, this.prevUrl);
 
-          this.screenReaderText.set('Paging', 'You are on page ' + 1 +
-            ' of ' + (Math.floor(this.memeLength / 6) + 1) +
-            ', showing ' + (this.memeLength >= 6 ? 6 : (this.memeLength % 6)) + ' memes.');
+          this.readPaging(1);
           this.readCurrentMemeSet();
           return this.allMemes;
         }
@@ -100,9 +130,39 @@ export class MyMemesComponent implements OnInit {
   }
 
   onTabChanged($event: MatTabChangeEvent): void {
+    this.currentTab = $event.index;
     this.currentPage = 0;
-    this.loadMemes($event.index);
+    this.loadMemes();
+  }
 
+  // Filter & Sort //
+  onFilterChanged(): void {
+    if (this.selectedFilter) {
+      if (this.filterType !== this.filterOptions.get(this.selectedFilter).type) {
+        this.filterValue = '';
+        this.filterType = this.filterOptions.get(this.selectedFilter).type;
+      }
+      else {
+        // Type stays the same (means only change in direction)
+        this.submitChanged();
+      }
+    }
+    else {
+      // Filter reset to None
+      this.filterValue = '';
+      this.filterType = '';
+      this.submitChanged();
+    }
+  }
+
+  submitChanged(): void {
+    this.currentPage = 0;
+    this.loadMemes();
+  }
+
+  // Dummy sort function to avoid maps get sorted alphabetic when using | keyvalue: asIsOrder
+  asIsOrder(a, b): number {
+    return 0;
   }
 
   // ScreenReader and its functions //
@@ -120,7 +180,15 @@ export class MyMemesComponent implements OnInit {
     return text;
   }
 
+  private readPaging(page: number): void {
+    if (!this.memeLength) { return; }
+    this.screenReaderText.set('Paging', 'You are on page ' + page +
+      ' of ' + (Math.floor(this.memeLength / 6) + 1) +
+      ', showing ' + (this.memeLength >= 6 ? 6 : (this.memeLength % 6)) + ' memes.');
+  }
   private readCurrentMemeSet(): void {
+    if (!this.allMemes) { this.screenReaderText.set('Amount', 'No Memes are available.'); return; }
+
     this.screenReaderText.set('Amount', this.memeLength + ' Memes are available.');
     let memeTitles = '';
     let countEmpty = 0;
