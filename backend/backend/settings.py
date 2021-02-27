ae955320033c0fe11ceefa77c7bd9b2dfd37e7b8
boehm-projects <moritz.boehm@gmail.com>
2021-02-27 04:30:19 +0100

    Removed Filterbackend option RestFramework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,8 +128,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    )
 }
 
 SIMPLE_JWT = {
